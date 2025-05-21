edd6de675a0bbbd64524fab182c32a663211b282
arthursmarinho <arthursnat@gmail.com>
2025-05-20 22:44:17 -0300

    update CORS configuration to allow requests from the production frontend

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://spigify-front.onrender.com/',
   });
 
   await app.listen(3000);
