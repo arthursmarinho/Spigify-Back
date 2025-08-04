4136fb9d7f9883ba9c06f1a91bfa69c9bebf10f2
arthursmarinho <arthursnat@gmail.com>
2025-08-04 09:20:11 -0300

    Enabled Cors methods

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { METHODS } from 'http';
+import { Post } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: ['https://spigify-front.onrender.com'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
