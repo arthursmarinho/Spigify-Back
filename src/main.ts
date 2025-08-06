9b19faa401d7f68f09f8d405afaa22ee79e6e407
arthursmarinho <arthursnat@gmail.com>
2025-08-06 15:00:21 -0300

    Insert localhost cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://spigify-front.onrender.com'],
+    origin: ['https://spigify-front.onrender.com', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
